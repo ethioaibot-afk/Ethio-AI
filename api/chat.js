88a95ebac5268e146d9aca0477610c86c366c302
ethioaibot-afk <ethioaibot@gmail.com>
2026-07-30 16:36:04 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -6,11 +6,17 @@ const ai = new GoogleGenAI({
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(405).json({ reply: "Method not allowed" });
   }
 
   try {
     const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({
+        reply: "Message is required."
+      });
+    }
 
     const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
@@ -20,10 +26,12 @@ export default async function handler(req, res) {
     res.status(200).json({
       reply: response.text,
     });
+
   } catch (error) {
     console.error(error);
+
     res.status(500).json({
-      reply: "❌ Error yeroo Gemini wajjin wal qunnamtii uumamu.",
+      reply: "❌ Error yeroo AI wajjin wal qunnamtii uumamu."
     });
   }
 }
